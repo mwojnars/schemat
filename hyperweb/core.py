c5e57e44e588fddb5eab461dcd884d562ff9aa89
Marcin Wojnarski <mwojnars@ns.onet.pl>
2020-02-28 22:22:27 +0100

    initial Django project

---
+++ hyperweb/core.py
@@ -0,0 +1,13 @@
+"""
+"""
+
+class Item:
+    """
+    """
+
+class Category(Item):
+    """
+    Specifition of a category of items, which is for items like a class for its instances.
+    Each category is an item itself.
+    """
+    
