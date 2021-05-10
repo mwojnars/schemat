c114b587e7d20be62b683cbf90d4127cf0878d7d
Marcin Wojnarski <>
2021-05-10 13:50:35 +0200

    initial tests added

---
+++ django-app/tests/tests.py
@@ -0,0 +1,45 @@
+#-*- coding: utf-8 -*-
+"""
+Run in "tests" folder:
+$
+$  py.test-3 -vW ignore::DeprecationWarning tests.py
+$
+
+"""
+
+import requests
+
+from hypertag import HyperHTML
+
+
+#####################################################################################################################################################
+#####
+#####  UTILITIES
+#####
+
+BASE_URL = "http://localhost:8001/"
+
+def get(relative_url):
+    return requests.get(BASE_URL + relative_url)
+
+def assert200(relative_url):
+    response = get(relative_url)
+    assert response.status_code == 200
+    
+
+#####################################################################################################################################################
+#####
+#####  TESTS
+#####
+
+def test_environment():
+    
+    response = requests.get("http://api.zippopotam.us/us/90210")
+    assert response.status_code == 200
+    
+
+def test_basic():
+
+    assert200("meta.category:0")
+    assert200("meta.item:1")
+    
