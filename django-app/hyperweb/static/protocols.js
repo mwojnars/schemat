ea7e0c90c132386266b9fa9addb443fe78832b22
Marcin Wojnarski <mwojnars@ns.onet.pl>
2021-08-18 23:51:31 +0200

    JS: generic_bind() replaced with class generic_protocol

--- django-app/hyperweb/static/protocols.js
+++ django-app/hyperweb/static/protocols.js
@@ -6,33 +6,59 @@ class ValueInteger extends Widget {
 
 }
 
-function generic_bind(widget) {
-    let view  = widget.querySelector("#view");
-    let edit  = widget.querySelector("#edit");
-
-    function show_edit() {
+class generic_protocol {
+    bind(widget) {
+        let view = widget.querySelector("#view");
+        let edit = widget.querySelector("#edit");
+        view.addEventListener('dblclick', () => this.show(view, edit));
+        edit.addEventListener('focusout', () => this.hide(view, edit));
+    }
+    show(view, edit) {
         //console.log('in show_edit()');
         view.style.display = 'none';
         edit.style.display = 'block';
         let focus = edit.querySelector(".focus");       // the element that should receive focus after form activation; can be missing
         if (focus) { focus.focus(); }
     }
-    function hide_edit() {
-        set_preview();
+    hide(view, edit) {
+        this.set_preview(view, edit);
         edit.style.display = 'none';
         view.style.display = 'block';
     }
-    function set_preview() {
+    set_preview(view, edit) {
         let input = edit.querySelector(".input");       // the (unique) element that contains a form value inserted by user
         view.textContent = input.value;
     }
-
-    view.addEventListener('dblclick', show_edit);
-    edit.addEventListener('focusout', hide_edit);
 }
 
+// function generic_bind(widget) {
+//     let view = widget.querySelector("#view");
+//     let edit = widget.querySelector("#edit");
+//
+//     function show_edit() {
+//         //console.log('in show_edit()');
+//         view.style.display = 'none';
+//         edit.style.display = 'block';
+//         let focus = edit.querySelector(".focus");       // the element that should receive focus after form activation; can be missing
+//         if (focus) { focus.focus(); }
+//     }
+//     function hide_edit() {
+//         set_preview();
+//         edit.style.display = 'none';
+//         view.style.display = 'block';
+//     }
+//     function set_preview() {
+//         let input = edit.querySelector(".input");       // the (unique) element that contains a form value inserted by user
+//         view.textContent = input.value;
+//     }
+//
+//     view.addEventListener('dblclick', show_edit);
+//     edit.addEventListener('focusout', hide_edit);
+// }
+
 let protocols = {
-    STRING: generic_bind,
+    STRING: new generic_protocol(),
+    // STRING: generic_bind,
 }
 
 function bind_all() {
@@ -40,8 +66,10 @@ function bind_all() {
     $("[protocol]").each(function (i, widget) {
         let name = widget.getAttribute('protocol');
         if (name in protocols) {
-            let bind = protocols[name];
-            bind(widget);
+            // let bind = protocols[name];
+            // bind(widget);
+            let proto = protocols[name];
+            proto.bind(widget);
         }
     });
 }
