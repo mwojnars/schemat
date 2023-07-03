54776fdb84a4b94df844adc943947f8b7cbb11d4
Marcin Wojnarski <mwojnars@ns.onet.pl>
2023-07-03 23:37:20 +0200

    minor

--- schemat/type.js
+++ schemat/type.js
@@ -217,7 +217,7 @@ Schema.Widget = class extends Widget {
         }
     }
 
-    empty(v)    { return T.isMissing(v) && I('missing') }       // view of an empty value, for display() and viewer()
+    empty(v)    { return T.isMissing(v) && I('undefined') }     // view of an empty value, for display() and viewer()
     view(v)     { return this.encode(v) }                       // view of a non-empty value, for display() and viewer()
     display(v)  { return this.empty(v) || this.view(v) }        // convert a value to a UI element for display in viewer()
     encode(v)   { return JSONx.stringify(v) }                   // convert a value to its editable representation
