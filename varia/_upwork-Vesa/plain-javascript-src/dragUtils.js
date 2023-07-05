1f3d834c9f52416f239fa94eb8c3c2d515bc0bbc
Marcin Wojnarski <mwojnars@ns.onet.pl>
2023-07-05 14:36:03 +0200

    some varia/* folder removed

--- varia/_upwork-Vesa/plain-javascript-src/dragUtils.js
+++
@@ -1,8 +0,0 @@
-// keep track of the insert order, in other words
-// this dictates should the dragged element be inserted
-// before or after the target element
-let insertOrder = null;
-export const setInsertOrder = (order) => {
-  insertOrder = order;
-};
-export const getInsertOrder = () => insertOrder;
--- varia/django-app/hyperweb/README.md
+++
@@ -1,5 +0,0 @@
-### Endpoints
-
-Types of endpoints:
-- _views_: present the data; no side effects (select)
-- _handlers_: perform operations on the data; have side effects (insert, update etc.)
