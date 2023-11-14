b873dbd11abf3b87565b90248b988bb58e43a6e4
Marcin Wojnarski <mwojnars@ns.onet.pl>
2023-11-14 22:11:15 +0100

    minor

--- schemat/cluster/local.js
+++ schemat/cluster/local.js
@@ -8,9 +8,10 @@ import {AsyncLocalStorage} from "async_hooks"
 /**********************************************************************************************************************/
 
 export function thread_local_variable() {
-    /* Create an async-thread local variable, X, using AsyncLocalStorage.
-       Call X.run_with(value, callback) on the returned object to execute callback() with the value of X
-       set to `value` within the current async-thread. The value is typically a Map or an object.
+    /* Creates an async-thread-local variable using AsyncLocalStorage. Returns the variable wrapped up in a Proxy,
+       so that every read/write access to a method or property is redirected to the current value of the variable.
+       Call run_with(value, callback) on the returned object to execute callback() with the variable's value
+       set to `value` within the current async thread; the `value` is typically a Map or an object.
      */
 
     const local = new AsyncLocalStorage()
@@ -24,7 +25,7 @@ export function thread_local_variable() {
 
             const store = local.getStore()
             return Reflect.get(store, prop, receiver)
-            
+
             // const value = store[prop]
             // return typeof value === 'function' ? value.bind(store) : value
         },
