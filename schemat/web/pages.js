bab4cbafa21545c9f38f80bfdb95ad910bdf6578
Marcin Wojnarski <mwojnars@ns.onet.pl>
2023-11-29 15:24:13 +0100

    minor

--- schemat/web/pages.js
+++ schemat/web/pages.js
@@ -247,8 +247,7 @@ export class ItemAdminPage extends ReactPage {
         Title() {
             /* <H1> element to be displayed as a page title. */
             let name = this.name
-            // let ciid = this.getStamp()
-            let ciid = delayed_render(Promise.resolve(this.getStamp()))
+            let ciid = this.getStamp()
             if (name)
                 return H1(name, ' ', SPAN({style: {fontSize:'40%', fontWeight:"normal"}, ...HTML(ciid)}))
             else
@@ -340,10 +339,8 @@ export class CategoryAdminPage extends ItemAdminPage {
             /* A single row in the list of items. */
             let name = item.name || item.getStamp({html:false})
             let url  = item.url()
-            // let url  = delayed_render(Promise.resolve(item.url()), [item])
             return TR(
                 TD(`${item._id_} ${NBSP}`),
-                // TD(A({href: url}, name), ' ', NBSP),
                 TD(url !== null ? A({href: url}, name) : `${name} (no URL)`, ' ', NBSP),
                 TD(BUTTON({onClick: () => remove(item)}, 'Delete')),
             )
