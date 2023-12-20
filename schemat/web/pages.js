00400852690cb0b3ea13489b9d2a67ecef22af3c
Marcin Wojnarski <mwojnars@ns.onet.pl>
2023-12-20 13:34:15 +0100

    cleanup

--- schemat/web/pages.js
+++ schemat/web/pages.js
@@ -303,7 +303,7 @@ export class CategoryAdminPage extends ItemAdminPage {
             if (side === 'server')
                 items = registry.scan_category(this)
             else
-                items = this.action.list_items()
+                items = this.action.list_items()        // TODO: must be pulled from response data on the client to avoid re-scanning on 1st render
                 // let items = await this.action.list_items().then(arr => T.amap(arr, item => item.load()))
 
             items = await T.arrayFromAsync(items).then(arr => T.amap(arr, item => item.load()))
@@ -311,23 +311,20 @@ export class CategoryAdminPage extends ItemAdminPage {
         }
 
         component({items: preloaded}) {
-            // let preloaded = this._context_.items               // TODO: must be pulled from response data on the client to avoid re-scanning on 1st render
-
             const scan = () => this.action.list_items()
             // const scan = () => registry.scan_category(this)         // returns an async generator that requires "for await"
 
-            const [items, setItems] = useState(preloaded || scan())          // existing child items; state prevents re-scan after every itemAdded()
-                                                                // TODO: use materialized list of items to explicitly control re-scanning
-                                                                //    ...and avoid React's incorrect refresh when Items (below) are called in a different way
+            const [items, setItems] = useState(preloaded || scan())     // existing child items; state prevents re-scan after every itemAdded()
+                                                                        // TODO: use materialized list of items to explicitly control re-scanning
+                                                                        //    ...and avoid React's incorrect refresh when Items (below) are called in a different way
 
-            const [newItems, setNewItems] = useState([])        // newly added items
+            const [newItems, setNewItems] = useState([])                // newly added items
             const itemAdded   = (item) => { setNewItems(prev => [...prev, item]) }
             const itemRemoved = (item) => { setNewItems(prev => prev.filter(i => i !== item)) }
 
             return super.component({extra: FRAGMENT(
                 H2('Items'),
-                e(preloaded ? this.ItemsLoaded : this.Items, {key: 'items', items: items, itemRemoved: async () => setItems(await scan())}),
-                // e(this.Items, {key: 'items', items: items, itemRemoved: () => setItems(scan())}),
+                e(this.ItemsLoaded, {key: 'items', items: items, itemRemoved: async () => setItems(await scan())}),
                 H3('Add item'),
                 e(this.Items, {items: newItems, itemRemoved}),
                 e(this.NewItem, {itemAdded}),
