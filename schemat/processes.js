b4d7edb117eddf044b5d2206ade852c537058c37
Marcin Wojnarski <mwojnars@ns.onet.pl>
2024-02-08 23:31:22 +0100

    no global `schemat` var

--- schemat/processes.js
+++ schemat/processes.js
@@ -6,10 +6,6 @@ import {set_global} from "./common/globals.js"
 export class SchematProcess {
     /* The main Schemat process, on a worker node or in a user browser. */
 
-    constructor() {
-        set_global({schemat: this})
-    }
-
     async _create_registry(registry_class, ...args) {
         let registry = new registry_class(...args)
         set_global({registry})
