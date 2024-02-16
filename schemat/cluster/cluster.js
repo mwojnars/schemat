e5ae157c5d2aaae49754e7a3f6a0b5f53efa4656
Marcin Wojnarski <mwojnars@ns.onet.pl>
2024-02-16 22:24:36 +0100

    db-cluster: created anew with all DB objects; relative filenames to local files

--- schemat/cluster/cluster.js
+++ schemat/cluster/cluster.js
@@ -15,28 +15,25 @@ export class Cluster { //extends Item {
         /* Load the bootstrap database & create the registry, then load this cluster's complete data from DB,
            which should replace the db object with the ultimate one (TODO).
          */
-        const mod_url  = await import('node:url')
-        const mod_path = await import('node:path')
+        // const mod_url  = await import('node:url')
+        // const mod_path = await import('node:path')
+        //
+        // const __filename = mod_url.fileURLToPath(import.meta.url)       // or: process.argv[1]
+        // const __dirname  = mod_path.dirname(__filename) + '/..'
+        //
+        // const DB_ROOT   = __dirname + '/data'
 
-        const __filename = mod_url.fileURLToPath(import.meta.url)       // or: process.argv[1]
-        const __dirname  = mod_path.dirname(__filename) + '/..'
-
-        const DB_ROOT   = __dirname + '/data'
-
-        // const cluster_ring_spec =
-        //     {file: DB_ROOT + '/db-cluster.yaml', start_id: 200, stop_id: 300}
+        const DB_ROOT   = './data'
 
         const ring_specs = [
-            {file: DB_ROOT + '/db-boot.yaml', start_id:    0, stop_id:  100, readonly: false},
-            {file: DB_ROOT + '/db-base.yaml', start_id:  100, stop_id: 1000, readonly: true},
-            {file: DB_ROOT + '/db-cluster.yaml', start_id: 200, stop_id: 300, readonly: false},
-            {file: DB_ROOT + '/../../app-demo/data/db-paperity.yaml', start_id: 1000, stop_id: null, readonly: true},
-            {file: DB_ROOT + '/db-demo.yaml', start_id: 1000, stop_id: null, readonly: true},
+            {file: DB_ROOT + '/db-boot.yaml',    start_id:    0, stop_id:  100, readonly: true},
+            {file: DB_ROOT + '/db-base.yaml',    start_id:  100, stop_id:  200, readonly: true},
+            {file: DB_ROOT + '/db-cluster.yaml', start_id:  200, stop_id:  300, readonly: false},
+            {file: DB_ROOT + '/db-demo.yaml',    start_id: 1000, stop_id: null, readonly: false},
+            // {file: DB_ROOT + '/../../app-demo/data/db-paperity.yaml', start_id: 1000, stop_id: null, readonly: true},
 
             // {item: 200},       // db-paperity.yaml
             // {item: 205},       // db-demo.yaml
-
-            // {item: 1015, name: 'mysql', readonly: true},
         ]
 
         // let req = new DataRequest(this, 'startup')
