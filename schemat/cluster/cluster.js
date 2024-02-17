8c02095d5a82aed94d0ad11882daf8d346018794
Marcin Wojnarski <mwojnars@ns.onet.pl>
2024-02-17 19:50:21 +0100

    Cluster: cleanup

--- schemat/cluster/cluster.js
+++ schemat/cluster/cluster.js
@@ -1,7 +1,5 @@
 import {print, T} from "../common/utils.js"
-import {Item} from "../item.js"
-import {Ring, Database} from "../db/db.js"
-import {DataRequest} from "../db/data_request.js";
+import {Database} from "../db/db.js"
 
 
 /**********************************************************************************************************************/
@@ -9,20 +7,10 @@ import {DataRequest} from "../db/data_request.js";
 export class Cluster { //extends Item {
     /* Managing the cluster's infrastructure. */
 
-    // db  (property)
-
     async startup() {
         /* Load the bootstrap database & create the registry, then load this cluster's complete data from DB,
            which should replace the db object with the ultimate one (TODO).
          */
-        // const mod_url  = await import('node:url')
-        // const mod_path = await import('node:path')
-        //
-        // const __filename = mod_url.fileURLToPath(import.meta.url)       // or: process.argv[1]
-        // const __dirname  = mod_path.dirname(__filename) + '/..'
-        //
-        // const DB_ROOT   = __dirname + '/data'
-
         let filename = './config.yaml'
 
         let fs = await import('node:fs')
@@ -32,23 +20,7 @@ export class Cluster { //extends Item {
         let config = yaml.parse(content)
         let rings = config.bootstrap_database.rings
 
-        print('config:', config)
-        print('rings:', rings)
-
         rings.forEach(ring => { if(ring.readonly === undefined) ring.readonly = true })
-
-        // const DB_ROOT   = './data'
-        //
-        // const ring_specs = [
-        //     {file: DB_ROOT + '/db-boot.yaml',    start_id:    0, stop_id:  100, readonly: true},
-        //     {file: DB_ROOT + '/db-base.yaml',    start_id:  100, stop_id:  200, readonly: true},
-        //     {file: DB_ROOT + '/db-cluster.yaml', start_id:  200, stop_id:  300, readonly: true},
-        //     {file: DB_ROOT + '/db-demo.yaml',    start_id: 1000, stop_id: null, readonly: true},
-        //     // {file: DB_ROOT + '/../../app-demo/data/db-paperity.yaml', start_id: 1000, stop_id: null, readonly: true},
-        //
-        //     // {item: 200},       // db-paperity.yaml
-        //     // {item: 205},       // db-demo.yaml
-        // ]
 
         let bootstrap_db = Database.create(rings)
         schemat.set_db(bootstrap_db)
