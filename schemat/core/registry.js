9a7ddc0ab68bfba30add3f6b46c9d91b0b254379
Marcin Wojnarski <mwojnars@ns.onet.pl>
2024-03-21 20:20:16 +0100

    Registry: ._cache > .objects

--- schemat/core/registry.js
+++ schemat/core/registry.js
@@ -6,24 +6,24 @@ import {ROOT_ID} from "../item.js";
 export class Registry {
     /* Process-local cache of web objects, records and indexes loaded from DB. */
 
-    _cache = new ObjectsCache()
+    objects = new ObjectsCache()
 
     _purging_now   = false
     _last_purge_ts = 0                  // timestamp of the last cache purge
 
 
-    has(id)         { return this._cache.has(id) }
-    get(id)         { return this._cache.get(id) }
+    has(id)         { return this.objects.has(id) }
+    get(id)         { return this.objects.get(id) }
 
     set(obj) {
         /* Put `obj` in the cache. This may override an existing instance with the same ID. */
         assert(obj._id_ !== undefined, `cannot register an object without an ID: ${obj}`)
         assert(!obj._meta_.mutable, `cannot register a mutable object: ${obj}`)
-        this._cache.set(obj._id_, obj)
+        this.objects.set(obj._id_, obj)
         return obj
     }
 
-    values()        { return this._cache.values() }
+    values()        { return this.objects.values() }
 
     async purge(min_delay) {
         /* Evict expired objects from the cache. */
@@ -36,7 +36,7 @@ export class Registry {
         const on_evict = (obj) => {
             if (obj._id_ === ROOT_ID) return schemat.reload(ROOT_ID)     // don't evict the root object
         }
-        await this._cache.evict_expired(on_evict)
+        await this.objects.evict_expired(on_evict)
 
         // if (!this.registry.has(ROOT_ID))            // if root category is no longer present in registry, call _init_root() once again
         //     await schemat._init_root()                 // WARN: between evict() and _init_root() there's no root_category defined! problem if a request comes in
