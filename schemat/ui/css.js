35808612cb5d5a2c5a549750832d480003c5bd48
Marcin Wojnarski <mwojnars@ns.onet.pl>
2024-03-28 14:45:32 +0100

    compact_css()

--- schemat/ui/css.js
+++ schemat/ui/css.js
@@ -11,6 +11,27 @@ let cssValidator = await tryimport('csstree-validator')
  **
  */
 
+export function compact_css(css) {
+    /* Remove comments and merge whitespace (including newlines) inside CSS code. */
+
+    let compacted = css.replace(/\/\*[\s\S]*?\*\//g, '')                        // remove comments
+
+    compacted = compacted.split(/(['"])(?:(?=(\\?))\2.)*?\1/)                           // avoid compacting whitespace inside quotes
+        .map((chunk, index) => index % 2 === 0 ? chunk.replace(/\s+/g, ' ') : chunk     // compact only outside quotes
+    ).join('')
+
+    compacted = compacted.replace(/\{\s+/g, '{').replace(/\s+\}/g, '}')         // remove spaces after "{" and before "}"
+
+    return compacted.trim()
+}
+
+
+/**********************************************************************************************************************
+ **
+ **  CSS SCOPING (custom solution)
+ **
+ */
+
 export function interpolate(strings, values) {
     /* Concatenate `strings` with `values` inserted between each pair of neighboring strings, for tagged templates.
        Both arguments are arrays; `strings` must be +1 longer than `values`.
