c2d8f30f91d3605ac26efac7e2a4363ce41ef1fe
Marcin Wojnarski <mwojnars@ns.onet.pl>
2024-08-09 13:06:12 +0200

    global flags: isNodeJS, isBrowser

--- schemat/common/globals.js
+++ schemat/common/globals.js
@@ -5,9 +5,22 @@
  */
 
 
+// global flags ...
+
+globalThis.isNodeJS =
+    typeof process !== 'undefined' &&               // `process` is a global object in Node.js but not in browsers
+    process.versions != null &&                     // process.versions contains Node.js-specific version information
+    process.versions.node != null                   // process.versions.node is the Node.js version string
+
+globalThis.isBrowser = !globalThis.isNodeJS
+
+
+
+// global objects ...
+
 export function set_global({schemat, Item} = {})
 {
-    /* This is a complete list of global variables defined by Schemat. */
+    /* This is a complete list of Schemat's global objects. */
 
     if (schemat)     globalThis.schemat = schemat
     if (Item)        globalThis.Item = Item
