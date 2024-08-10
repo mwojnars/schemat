7b73b90c5f1bc99650d304a0a51fbb08b1b5c505
Marcin Wojnarski <mwojnars@ns.onet.pl>
2024-08-10 13:12:22 +0200

    scoping plugin rewritten and fixed; processNode()

--- schemat/std/transforms.js
+++ schemat/std/transforms.js
@@ -10,63 +10,115 @@ import postcssMixins from "postcss-mixins"
 import postcssSimpleVars from "postcss-simple-vars"
 import postcssCustomProperties from "postcss-custom-properties"
 
+import {print} from "../common/utils.js"
 
 
 function postcssScoping() {
     return {
-        postcssPlugin: "scoping-plugin",
+        postcssPlugin: "scope-beyond-plugin",
         Root(root) {
-            let currentWidgetName = null
-            let nestedLevel = 0
-
-            // root.walkComments(comment => {
-            //     const match = comment.text.match(/^\s*widget:\s*(\w+)\s*$/)
-            //     if (match) currentWidgetName = match[1]
-            // })
-
-            root.walk(node => {
-                if (node.type === "rule") {
-                    if (nestedLevel === 0 && node.selector.startsWith("."))
-                        currentWidgetName = node.selector.slice(1)
-
-                    if (currentWidgetName)
-                        processRule(node, currentWidgetName)
-
-                    nestedLevel++
-                }
-                else if (node.type === "decl")
-                    nestedLevel = 0
-            })
+            return processNode(root)
         }
+
+        // Root(root) {
+        //     let widget = null
+        //     let stack = []
+        //     let top = () => stack[stack.length - 1]
+        //
+        //     // root.walkComments(comment => {
+        //     //     const match = comment.text.match(/^\s*widget:\s*(\w+)\s*$/)
+        //     //     if (match) widget = match[1]
+        //     // })
+        //
+        //     root.walk(node => {
+        //         // if (node.type === "rule") {
+        //         //     if (level === 0 && node.selector.startsWith("."))
+        //         //         widget = node.selector.slice(1)
+        //         //
+        //         //     else if (widget)
+        //         //         processRule(node, widget)
+        //         //
+        //         //     level++
+        //         // }
+        //         // else if (node.type === "decl")
+        //         //     level = 0
+        //
+        //         if (node.type === "rule") {
+        //             if (stack.length === 0 && node.selector.startsWith("."))    // if top-level rule, update current widget
+        //                 widget = node.selector.slice(1)
+        //
+        //             if (widget) processRule(node, widget)
+        //
+        //             stack.push(node)                                // push current node to stack to increase nesting level
+        //             print('pushed:', node.selector, stack.length)
+        //         }
+        //         else if (node.type === "decl")                      // do nothing on declaration, but ensure we manage the stack correctly
+        //             while (stack.length && stack[stack.length - 1].type !== "rule")
+        //                 stack.pop()
+        //
+        //         if (node.type === "rule") {                         // when a node is fully processed, we pop it from the stack
+        //             stack.pop()
+        //             if (stack.length === 0)                         // if stack becomes empty, reset widget
+        //                 widget = null
+        //             else                                            // update widget based on the top of the stack
+        //                 widget = top().selector.slice(1)
+        //         }
+        //     })
+        // }
+    }
+    
+    function processNode(node, widget = null, scope_char = "|") {
+        /* Recursively process the `node` and its descendants to replace the scoping symbol, "|", in each rule with
+           `:not(.after-${widget} *)` qualifier to protect inner widgets from being spoiled by an outer widget's style.
+           Update the `widget` name along the way.
+
+           The `.after-${widget}` class name should be added to the inner widgets' container elements in the HTML.
+           Typically, this plugin should be combined with the `postcss-nested` plugin to handle nested rules correctly,
+           and the latter should be placed AFTER this plugin in the list of PostCSS plugins.
+         */
+
+        if (node.type === "rule" && widget)
+            node.selector = node.selector.replace(scope_char, `:not(.after-${widget} *)`)
+
+        if (node.type === "rule" && node.selector.startsWith(".")) {
+            widget = node.selector.slice(1).split(" ")[0].split(":")[0]
+            print("widget:", widget)
+        }
+
+        for (let child of node.nodes || [])
+            processNode(child, widget)
     }
 
-    function processRule(rule, widgetName) {
-        let parts = rule.selector.split(",")
-
-        parts = parts.map(part => {
-            part = part.trim()
-
-            if (part.includes("|"))
-                part = part.replace("|", `:not(.after-${widgetName} *)`)
-
-            if (!part.startsWith(`.${widgetName}`))
-                part = `.${widgetName} ${part}`
-
-            return part
-        })
-
-        rule.selector = parts.join(", ")
-    }
+    // function processRule(rule, widget, scope_char) {
+    //     let parts = rule.selector.split(",")
+    //
+    //     parts = parts.map(part => {
+    //         part = part.trim()
+    //
+    //         // add the "after-widget" protection to avoid spoiling inner widgets with this widget's style;
+    //         // IMPORTANT: this prevents the recursive embedding of the widget in itself!
+    //         if (part.includes(scope_char))
+    //             part = part.replace(scope_char, `:not(.after-${widget} *)`)
+    //
+    //         // NOTE: the code below may only be needed when postcssNested() plugin is NOT enabled
+    //         // if (!part.startsWith(`.${widget}`))
+    //         //     part = `.${widget} ${part}`
+    //
+    //         return part
+    //     })
+    //
+    //     rule.selector = parts.join(", ")
+    // }
 }
 
 export async function transform_postcss(css, filepath) {
     const result = await postcss([
+        postcssScoping(),
         postcssImport(),
         postcssMixins(),
         postcssNested(),
         postcssSimpleVars(),
         postcssCustomProperties(),
-        postcssScoping(),
     ]).process(css, {from: filepath})
     return result.css
 }
