cd5704e1fef2567fda3f2774279393edf482b7a1
Marcin Wojnarski <mwojnars@ns.onet.pl>
2024-08-21 15:15:52 +0200

    html_page() web adapter - draft

--- schemat/web/adapters.js
+++ schemat/web/adapters.js
@@ -1,13 +1,33 @@
 /*
     Web adapters are functions that create handlers for HTTP requests.
+    They can only be used on the server side. If executed on the client, they will throw errors.
  */
 
 
+import { tryimport } from '../common/utils'
+
+// conditionally import the modules
+let fs = await tryimport('node:fs')
+let ejs = await tryimport('ejs')
+
+
+/**********************************************************************************************************************/
+
 export function html_page(filename, locals = {}, opts = {}) {
-    /* Return a function that generates an HTML page: either from a static .html file, or from a template (.ejs);
-       in the latter case, the template is rendered with `locals` as its variables.
+    /* Returns a function that loads an HTML page: either from a static .html file or from a template (.ejs);
+        in the latter case, the template is rendered with `locals` as its variables.
      */
-    return function html_generate(request) {
-        // `request` is the Schemat's custom Request object
+    return () => {
+        // check the file type by extension... load/render the file accordingly...
+        const ext = filename.includes('.') ? filename.split('.').pop().toLowerCase() : 'html'
+        
+        if (ext === 'html' || ext === 'htm')
+            return fs.readFileSync(filename, 'utf-8')
+        
+        else if (ext === 'ejs') {
+            const template = fs.readFileSync(filename, 'utf-8')
+            return ejs.render(template, locals, opts)
+        } 
+        throw new Error(`Unsupported file type: ${ext}`)
     }
 }
