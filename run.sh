8e5a5844248d39ed059171b28434768b29f799f5
Marcin Wojnarski <mwojnars@ns.onet.pl>
2024-08-27 11:43:51 +0200

    minor

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Get the directory of the current script
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Execute Schemat from the root folder of the source code
-cd $SCRIPT_DIR
-node --experimental-vm-modules "$SCRIPT_DIR/schemat/server/run.js" "$@"
+cd $ROOT_DIR
+node --experimental-vm-modules "$ROOT_DIR/schemat/server/run.js" "$@"
