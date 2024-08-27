13bc2077be0c9a401b9169865c160614a0cc7c46
Marcin Wojnarski <mwojnars@ns.onet.pl>
2024-08-27 13:01:02 +0200

    setup_demo_01(): draft

--- schemat/install/demo.js
+++ schemat/install/demo.js
@@ -7,6 +7,8 @@ import path from 'path'
 import yaml from 'js-yaml'
 
 
+/**********************************************************************************************************************/
+
 let __filename = fileURLToPath(import.meta.url)
 let __dirname  = path.dirname(__filename)
 
@@ -14,13 +16,29 @@ let root_dir = `${__dirname}/../..`
 
 
 function _load_data_init() {
-    /* Read db-init.yaml and return its plain-text content. */
+    /* Read db-init.yaml and return its plain-text content. Drop unneeded objects. */
 
-    let db_init_path = `${root_dir}/schemat/data/db-init.yaml`
-    let db_init_content = fs.readFileSync(db_init_path, 'utf8')
-    // let db_init_data = yaml.load(db_init_content)
-    return db_init_content
+    let path = `${root_dir}/schemat/data/db-init.yaml`
+    let db = fs.readFileSync(path, 'utf8')
+    // let data = yaml.load(db)
+
+    let ids = [1005, 1006, 1016, 1017, 1018, 1029, 1030, 1031]
+    for (let id of ids) db = _delete_object(db, id)
+
+    return db
 }
+
+function _delete_object(db, id) {
+    /* In a yaml string `db`, delete the block that starts with:
+         - __id: <id>
+           ...
+        up until the next `- __id:` line.
+     */
+    let re = new RegExp(`^- __id: ${id}(.*?)\n(?=- __id:|$)`, 'ms')
+    return db.replace(re, '')
+}
+
+/**********************************************************************************************************************/
 
 export function setup_demo_01() {
     // load initial `db` from db-init.yaml
@@ -28,10 +46,10 @@ export function setup_demo_01() {
     let demo_dir = `${root_dir}/demo/${demo_name}`
     let db = _load_data_init()
     
-    // replace file paths and object names in `db`, drop unneeded objects
+    // replace file paths and object names in `db`
     db = db.replace('/schemat/data/db-init.', `/demo/${demo_name}/db.`)
     db = db.replace('db-init', `db`)
-    db = db.replace('main-site', `Books - Demo Site`)
+    db = db.replace('main-site', `Books Demo`)
 
     // save as db.yaml in the demo folder
     fs.writeFileSync(`${demo_dir}/db.yaml`, db, 'utf8')
