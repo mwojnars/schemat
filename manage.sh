8824f6fe3bf11b042e3b7410174f8026abf4fbeb
Marcin Wojnarski <mwojnars@ns.onet.pl>
2024-08-27 14:46:56 +0200

    manage.sh

---
+++ manage.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Get the directory of the current script
+ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Execute Schemat from the root folder of the source code
+cd $ROOT_DIR
+node --experimental-vm-modules "$ROOT_DIR/schemat/server/manage.js" "$@"
