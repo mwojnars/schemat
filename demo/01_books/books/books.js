eac9439df394853fe82f782628d284f23d40b0f3
Marcin Wojnarski <mwojnars@ns.onet.pl>
2024-09-06 14:34:12 +0200

    minor

--- demo/01_books/books/books.js
+++ demo/01_books/books/books.js
@@ -4,6 +4,6 @@ import {Category} from "../../../schemat";
 export class Books extends Category {
 
     GET__view() {
-        return 'TODO: Book List ...'
+        return 'Books List ...'
     }
 }
