d4b1d0778ef2fbf0d15767afd905ee226d311def
Marcin Wojnarski <mwojnars@ns.onet.pl>
2024-09-10 19:39:13 +0200

    demo: list of authors includes "in store" references

--- demo/01_booksy/authors/author.js
+++ demo/01_booksy/authors/author.js
@@ -4,12 +4,16 @@ import {html_page} from "../../../schemat/web/adapters.js"
 export class Author extends schemat.Item {
 
     static async GET__view() {
-        let authors = []
+        let authors = [], books = []
         for await (const author of schemat.scan_category(this)) {
             await author.load()
             authors.push(author)
         }
+        for await (const book of schemat.scan_category(5000)) {
+            await book.load()
+            books.push(book)
+        }
         let path = import.meta.resolve('./authors.ejs')
-        return html_page(path, {authors})
+        return html_page(path, {authors, books})
     }
 }
