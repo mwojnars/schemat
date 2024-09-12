5d6a77fc3eca1993e1d8981eb97b0c15899979bf
Marcin Wojnarski <mwojnars@ns.onet.pl>
2024-09-12 19:37:30 +0200

    minor

--- demo/01_booksy/authors/author.js
+++ demo/01_booksy/authors/author.js
@@ -1,12 +1,10 @@
 import {html_page} from "../../../schemat/web/adapters.js"
 
 
-// BookCategory     -- loaded automatically as a global object from [site.global] property
-
-
 export class Author extends schemat.Item {
 
     static async GET__view() {
+        // BookCategory below is a global object: declared in [site.global] in DB and loaded automatically during site initialization
         let authors = await this.list_objects({load: true})
         let books = await BookCategory.list_objects({load: true})
         let path = import.meta.resolve('./authors.ejs')
