d3a02ee84ee4e869263884ba886a7c616c8a5266
Marcin Wojnarski <mwojnars@ns.onet.pl>
2024-09-15 00:30:31 +0200

    demo updated

--- schemat/install/demo.js
+++ schemat/install/demo.js
@@ -55,8 +55,8 @@ async function create_demo_01() {
     db = db.replaceAll('/app', `/demo/${demo_name}`)
 
     // insert AuthorCategory and BookCategory references in [site.global]
-    db = db.replace(`global:`, `global:\n    AuthorCategory:\n      "@": 5001\n    BookCategory:\n      "@": 5000`)
-    db = db.replace(`entries:`, `entries:\n    authors:\n      "@": 5001\n    books:\n      "@": 5000\n    book:\n      "@": 5014`)
+    db = db.replaceAll(`global:`, `global:\n    AuthorCategory:\n      "@": 5001\n    BookCategory:\n      "@": 5000`)
+    db = db.replaceAll(`entries:`, `entries:\n    authors:\n      "@": 5001\n    books:\n      "@": 5000\n    book:\n      "@": 5014`)
 
     // save as db-site.yaml in the demo folder
     fs.writeFileSync(`${demo_dir}/_data/db-site.yaml`, db, 'utf8')
