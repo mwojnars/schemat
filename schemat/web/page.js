1d61908cabc8200d90943937553ea31a95daa9d9
Marcin Wojnarski <mwojnars@ns.onet.pl>
2024-09-17 20:30:26 +0200

    class Page - draft

---
+++ schemat/web/page.js
@@ -0,0 +1,12 @@
+import {Item} from "../core/item.js"
+
+
+/**********************************************************************************************************************/
+
+export class Page extends Item {
+    /* Class for the [Page] category of objects that represent standalone web pages. */
+
+    // properties...
+    view_endpoint
+
+}
