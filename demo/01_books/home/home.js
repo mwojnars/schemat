b1f07cfaab645993adff9296e7221be1913d117c
Marcin Wojnarski <mwojnars@ns.onet.pl>
2024-09-17 23:33:59 +0200

    demo: features objects selected randomly, max 6

--- demo/01_books/home/home.js
+++ demo/01_books/home/home.js
@@ -1,12 +1,23 @@
 import {html_page} from "../../../schemat/web/adapters.js"
 
 
+function select_random(array, count) {
+    /* Randomly select `count` elements from `array`. */
+    return array.sort(() => Math.random() - 0.5).slice(0, count)
+}
+
+
 export async function homepage() {
     let authors = await AuthorCategory.list_objects({load: true})
     let books = await BookCategory.list_objects({load: true})
-    for (let book of books)
+
+    let featured_authors = select_random(authors, 6)
+    let featured_books = select_random(books, 6)
+
+    for (let book of featured_books)
         for (let author of book.author$) await author.load()
 
     let path = import.meta.resolve('./home.ejs')
-    return html_page(path, {featured_books: books, featured_authors: authors})
+    return html_page(path, {featured_books, featured_authors})
 }
+
