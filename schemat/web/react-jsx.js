d7d53748ce072a1800fda77f9c9602b454840e71
Marcin Wojnarski <mwojnars@ns.onet.pl>
2024-10-10 20:26:46 +0200

    transpileJSX() improved

--- schemat/web/react-jsx.js
+++ schemat/web/react-jsx.js
@@ -6,13 +6,27 @@ import {readFileSync} from 'fs'
 import {join} from 'path'
 import {transformAsync} from '@babel/core'
 
-// Function to transpile JSX to JS
-export async function transpileJSX(filePath){
+export async function transpileJSX(path, {short = true} = {}) {
+    /* Function to transpile JSX to JS. */
     try {
-        let jsx = readFileSync(filePath, 'utf8')        // read the file contents
+        let jsx = readFileSync(path, 'utf8')            // read the file contents
         let {code} = await transformAsync(jsx, {        // use Babel to transpile JSX to JavaScript
             presets: ['@babel/preset-react'],           // preset for transpiling JSX
         })
+
+        // optionally define `el()` shortcut and replace React.createElement() with el()
+        let alias = 'el'
+        let exist = new RegExp(`\\b${alias}\\b`)
+        if (short && !exist.test(code)) {
+            code = code.replace(/React\.createElement/g, alias)
+            // code = `let ${alias} = React.createElement;\n${code}`
+
+            // regex to match the header block: empty lines, comments, import statements
+            let header = /^(\s*(\/\/[^\n]*|\/\*[\s\S]*?\*\/|\s*import[^\n]*))*\n?/
+
+            // insert the alias after the header block, so that all imports execute beforehand
+            code = code.replace(header, match => match + '\nlet el = React.createElement;\n')
+        }
         return code
 
     } catch (error) {
@@ -21,7 +35,8 @@ export async function transpileJSX(filePath){
     }
 }
 
-// Example usage:
-let file = join(process.cwd(), 'example.jsx')
-transpileJSX(file).then(js => {console.log('Transpiled JS code:', js)})
-
+function example() {
+    // example usage:
+    let file = join(process.cwd(), 'example.jsx')
+    transpileJSX(file).then(js => {console.log('Transpiled JS code:', js)})
+}
