db0c529350417aad7f3d4093271defc4b24d6d91
Marcin Wojnarski <mwojnars@ns.onet.pl>
2024-10-14 02:07:22 +0200

    [Revision].class configured

--- schemat/core/revision.js
+++ schemat/core/revision.js
@@ -8,6 +8,7 @@ export class Revision extends WebObject {
 
     restore() {
         /* Recreate the target object in the form represented by this revision. */
+        print(`Revision.restore(): rev [${this.id}] for target [${this.target}]`)
         let id  = this.target.id
         return WebObject.from_json(id, this.data, {mutable: false})
     }
