be969d7be5d8fe88e156a73fe2d8e65ea877d419
Marcin Wojnarski <mwojnars@ns.onet.pl>
2024-10-21 21:32:53 +0200

    Category.list_objects() uses GET.*() not POST.*() endpoint

--- schemat/core/category.js
+++ schemat/core/category.js
@@ -60,7 +60,7 @@ export class Category extends WebObject {
 
     async list_objects(opts = {}) {
         /* Return an array of all objects in this category, possibly truncated or re-ordered according to `opts`. */
-        return this.POST.list_objects(opts)
+        return this.GET.list_objects(opts)
     }
 
     _get_handler(endpoint) {
