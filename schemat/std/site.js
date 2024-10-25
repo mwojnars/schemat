3915fef6c81d137870a7f42463a99af0f3daf3eb
Marcin Wojnarski <mwojnars@ns.onet.pl>
2024-10-25 14:04:50 +0200

    minor

--- schemat/std/site.js
+++ schemat/std/site.js
@@ -175,8 +175,8 @@ export class Site extends WebObject {
     }
 
     'POST.submit_edits'() {
-        /* Submit a list of object edits to the DB. Each plain edit is an array: [op, ...args],
-           where `op` is the name of the EDIT_* operation to be executed, and `args` are 0+ arguments to be passed to the operation.
+        /* Submit a list of object edits to the DB. Each plain edit is an array: [op, ...args], where `op` is the name
+           of the edit.<name>() operation to be executed, and `args` are 0+ arguments to be passed to the operation.
          */
         return new JsonPOST({
             server: (id, ...edits) => this.database.update(id, ...edits),
