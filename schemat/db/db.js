dfebdfc74abdb78728b09a7f48226fbb6b8644fd
Marcin Wojnarski <mwojnars@ns.onet.pl>
2024-12-16 20:29:51 +0100

    Ring & Database: .scan_index() > .scan()

--- schemat/db/db.js
+++ schemat/db/db.js
@@ -157,8 +157,8 @@ export class Ring extends WebObject {
 
     /***  Indexes and Transforms  ***/
 
-    async *scan_index(name, {start, stop, limit=null, reverse=false, batch_size=100} = {}) {
-        /* Scan an index `name` in the range [`start`, `stop`) and yield the results.
+    async *scan(name, {start, stop, limit=null, reverse=false, batch_size=100} = {}) {
+        /* Scan a given data stream, `name`, in the range [`start`, `stop`) and yield the results.
            If `limit` is not null, yield at most `limit` items.
            If `reverse` is true, scan in the reverse order.
            If `batch_size` is not null, yield items in batches of `batch_size` items.
@@ -333,9 +333,9 @@ export class Database extends WebObject {
 
     /***  Indexes  ***/
 
-    async *scan_index(name, {offset, limit, ...opts} = {}) {
+    async *scan(name, {offset, limit, ...opts} = {}) {
         /* Yield a stream of plain Records from the index, merge-sorted from all the rings. */
-        let streams = this.rings.map(r => r.scan_index(name, opts))
+        let streams = this.rings.map(r => r.scan(name, opts))
         let merged = merge(Record.compare, ...streams)
         
         if (offset)
