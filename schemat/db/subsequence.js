e61a6f537061563798b2a2a64f2b4ba931f2019d
Marcin Wojnarski <mwojnars@ns.onet.pl>
2024-12-18 16:43:40 +0100

    Subsequence class moved to subsequence.js

---
+++ schemat/db/subsequence.js
@@ -0,0 +1,60 @@
+/* Subsequence class is NOT USED right now! */
+
+import {INTEGER} from "../types/type.js";
+import {BinaryInput} from "../common/binary.js";
+
+
+export class Subsequence {
+    /* A sequence of binary key-value pairs that is physically stored as a subsequence of another Sequence, with keys prefixed
+       by a constant: the ID of the Operator that produced this subsequence. As a thin wrapper around the underlying
+       physical (sub)sequence, this class is NOT stored in the DB, and does NOT inherit from Sequence nor WebObject.
+     */
+
+    base_sequence               // the underlying Sequence
+    id                          // ID of the Operator that produced this subsequence
+
+    static iid_type = new INTEGER({blank: false})       // for encoding/decoding the ID using variable-length encoding
+
+    constructor(id, base_sequence) {
+        this.base_sequence = base_sequence
+        this.id = id
+        this.prefix = Subsequence.iid_type.encode_uint(id)
+    }
+
+    async put(req) {
+        let prefixed_key = this._prefix_key(req.args.key)
+        let modified_req = req.safe_step(this, 'put', {...req.args, key: prefixed_key})
+        return this.base_sequence.put(modified_req)
+    }
+
+    async del(req) {
+        let prefixed_key = this._prefix_key(req.args.key)
+        let modified_req = req.safe_step(this, 'del', {...req.args, key: prefixed_key})
+        return this.base_sequence.del(modified_req)
+    }
+
+    async* scan_binary(opts = {}) {
+        let start = opts.start ? this._prefix_key(opts.start) : null
+        let stop = opts.stop ? this._prefix_key(opts.stop) : null
+
+        let base_scan = this.base_sequence.scan_binary({...opts, start, stop})
+
+        for await (let [key, value] of base_scan)
+            yield [this._unprefix_key(key), value]
+    }
+
+    _prefix_key(key) {
+        let result = new Uint8Array(this.prefix.length + key.length)
+        result.set(this.prefix, 0)
+        result.set(key, this.prefix.length)
+        return result
+    }
+
+    _unprefix_key(prefixed_key) {
+        let input = new BinaryInput(prefixed_key)
+        let id = Subsequence.iid_type.decode_uint(input)
+        if (id !== this.id) throw new Error(`Invalid subsequence key, found ID prefix=${id} instead of ${this.id} in key ${prefixed_key}`)
+        return input.current()
+    }
+}
+
