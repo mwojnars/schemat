4166a950a60e619264f29ac684cce4e92b116410
Marcin Wojnarski <mwojnars@ns.onet.pl>
2024-12-28 15:30:40 +0100

    DataBlock._save() performs _annotate() on result data

--- schemat/db/block.js
+++ schemat/db/block.js
@@ -156,7 +156,7 @@ export class DataBlock extends Block {
     }
 
     _annotate(json) {
-        /* Append metadata (ring & block ID) to the JSON content of an object retrieved during select/update. */
+        /* Append metadata (__meta) with ring & block ID to the JSON content of an object retrieved during select/update. */
         let plain = JSON.parse(json)
         plain.__meta = {ring: this.ring.id, block: this.id}
         return JSON.stringify(plain)
@@ -302,6 +302,8 @@ export class DataBlock extends Block {
 
         await this._put(key, data)
         await this.propagate_change(key, prev, obj)
+
+        data = this._annotate(data)
         return schemat.register_record({id, data})
     }
 
