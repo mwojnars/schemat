6cc25ff4b72b69c28b9179abeec54b362481bf58
Marcin Wojnarski <mwojnars@ns.onet.pl>
2025-01-20 23:56:08 +0100

    comments

--- schemat/server/agent.js
+++ schemat/server/agent.js
@@ -2,12 +2,13 @@ import {WebObject} from "../core/object.js"
 
 
 export class Agent extends WebObject {
-    /* A web object that can be installed on a particular machine(s) in the cluster to run a perpetual operation there.
-       Typically, the agent runs a web server, or a microservice of any kind, with a perpetual event loop.
-       The agent is allowed to use local resources of the host machine: files, sockets, etc.
+    /* A web object that can be installed on a particular machine(s) in the cluster to run a perpetual operation there (a microservice).
+       Typically, the agent runs a web server, or an intra-cluster microservice of any kind, with a perpetual event loop.
+       The agent is allowed to use local resources of the host machine: files, sockets, etc.; with some of them (typically files)
+       being allocated/deallocated in __install__/__uninstall__(), while some others (e.g., sockets) in __start__/__stop__().
     */
 
-    // __meta.state     -- the state object returned by __start__()
+    // __meta.state     -- the state object returned by __start__(), to be passed to __stop__() when the microservice is to be terminated
 
     async __start__()     {}
     async __stop__(state) {}
