b38fa248806e2831728a63b9c032f7393b97d8e4
Marcin Wojnarski <mwojnars@ns.onet.pl>
2025-01-24 01:12:02 +0100

    cleanup

--- schemat/core/schemat.js
+++ schemat/core/schemat.js
@@ -259,19 +259,11 @@ export class Schemat {
     }
 
     async reload(id, strict = false) {
-        /* Load contents into an existing stub, or create a new instance of the object using the most recent record from the registry,
-           or download the record from DB. When the object is fully initialized replace the existing instance in the registry. Return the object.
-           If strict=true, a new instance is created in every case.
+        /* Load contents into an existing stub, or create a new instance of the object using the most recent record from the registry
+           (download from DB if missing). When the object is fully initialized replace the existing instance in the registry. Return the object.
+           If strict=true, a new instance is always created.
          */
         assert(id)
-
-        // if (strict) {
-        //     let prev = this.get_object(id)
-        //     let stub = WebObject.stub(id)
-        //     if (prev) return stub.load().then(() => this.registry.set_object(stub))
-        //     else return this.registry.set_object(stub).load()
-        // }
-
         let loading = this._loading.get(id)
         if (loading && !strict) return loading
 
