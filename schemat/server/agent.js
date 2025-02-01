0bd62be8ffd86e6f7199317b445efa8567247742
Marcin Wojnarski <mwojnars@ns.onet.pl>
2025-02-02 00:39:39 +0100

    Agent.hard_restart

--- schemat/server/agent.js
+++ schemat/server/agent.js
@@ -59,7 +59,7 @@ export class Agent extends WebObject {
     // __num_workers    -- 0/1/N, the number of concurrent workers per node that should execute this agent's loop at the same time; 0 = "all available"
     // __state          -- the state object returned by __start__(), to be passed to __stop__() when the microservice is to be terminated
 
-    quick_restart
+    hard_restart
 
     async __install__(node) {}      // ideally, this method should be idempotent in case of failure and subsequent re-launch
     async __uninstall__(node) {}
@@ -70,9 +70,9 @@ export class Agent extends WebObject {
     async __restart__(state, prev) {
         /* In many cases, refreshing an agent in the worker process does NOT require full stop+start, which might have undesired side effects
            (temporary unavailability of the microservice). For this reason, __restart__() is called upon agent refresh - it can be customized
-           in subclasses, and the default implementation either does nothing (if quick_restart=true), or performs the full stop+start cycle.
+           in subclasses, and the default implementation either does nothing (default), or performs the full stop+start cycle (if hard_restart=true).
          */
-        if (this.quick_restart) return state
+        if (!this.hard_restart) return state
         await prev.__stop__(state)
         return this.__start__()
     }
