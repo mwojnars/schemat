00f844a39ea8fb699447674ef621b9617c31faea
Marcin Wojnarski <mwojnars@ns.onet.pl>
2025-02-14 00:09:11 +0100

    Logger: KAFKA.log() draft

--- schemat/server/logger.js
+++ schemat/server/logger.js
@@ -1,9 +1,23 @@
-import {KafkaAgent} from "./kafka.js";
+import {assert, print, utc} from '../common/utils.js'
+import {JsonKAFKA, KafkaAgent} from "./kafka.js";
 
 
 export class Logger extends KafkaAgent {
     /* Receives debug messages in its private Kafka topic. Performs post-processing, persistence,
        and periodical clean-up of old messages.
      */
+
+    'KAFKA.log'() {
+        return new JsonKAFKA({
+            server: async (msg, args = null, level = 'INFO') => {
+                if (args) {
+                    let list = Object.entries(args).map(([k, v]) => k + `=${JSON.stringify(v)}`).join(', ')
+                    if (list) msg = `${msg} | ${list}`
+                }
+                print(`[${utc()}] ${level.padStart(5)}: ${msg}`)
+            }
+        })
+    }
+
 }
 
