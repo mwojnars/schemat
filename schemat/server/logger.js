830faf2d45a4beffb16000bc908967f7e4df92a8
Marcin Wojnarski <mwojnars@ns.onet.pl>
2025-02-20 13:25:10 +0100

    minor

--- schemat/server/logger.js
+++ schemat/server/logger.js
@@ -7,17 +7,28 @@ export class Logger extends KafkaAgent {
        and periodical cleanup of old messages.
      */
 
-    'KAFKA.log'() {
-        return new JsonKAFKA({
-            server: async (msg, args = null, level = 'INFO') => {
-                if (args) {
-                    let list = Object.entries(args).map(([k, v]) => k + `=${JSON.stringify(v)}`).join(', ')
-                    if (list) msg = `${msg} | ${list}`
-                }
-                print(`[${utc()}] ${level.padStart(5)}: ${msg}`)
-            }
-        })
+    log(msg, args = null, level = 'INFO') {
+        /* TODO: this method will write directly to a log-file object in the DB,
+                 so that decentralized writes to multiple active log-files is possible.
+         */
+        if (args) {
+            let list = Object.entries(args).map(([k, v]) => k + `=${JSON.stringify(v)}`).join(', ')
+            if (list) msg = `${msg} | ${list}`
+        }
+        print(`[${utc()}] ${level.padStart(5)}: ${msg}`)
     }
+
+    // 'KAFKA.log'() {
+    //     return new JsonKAFKA({
+    //         server: async (msg, args = null, level = 'INFO') => {
+    //             if (args) {
+    //                 let list = Object.entries(args).map(([k, v]) => k + `=${JSON.stringify(v)}`).join(', ')
+    //                 if (list) msg = `${msg} | ${list}`
+    //             }
+    //             print(`[${utc()}] ${level.padStart(5)}: ${msg}`)
+    //         }
+    //     })
+    // }
 
 }
 
