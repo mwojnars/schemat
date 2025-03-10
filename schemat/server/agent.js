6e3b3c2dc73424a7bb48befb30495d5f96bcb256
Marcin Wojnarski <mwojnars@ns.onet.pl>
2025-03-10 21:43:08 +0100

    Agent: __num_workers > num_workers

--- schemat/server/agent.js
+++ schemat/server/agent.js
@@ -16,8 +16,8 @@ export class Agent extends WebObject {
 
     // __node __node$       -- node(s) where this agent is installed & running as a leader
     // __replica __replica$ -- node(s) where this agent is installed & running as a replica
-    // __num_workers        -- 0/1/N, the number of concurrent workers per node that should execute this agent's loop at the same time; 0 = "all available"
 
+    num_workers     // 0/1/N, the number of concurrent workers per node that should execute this agent's loop at the same time; 0 = "all available"
     hard_restart
 
     async __install__(node) {}  // ideally, this method should be idempotent in case of failure and subsequent re-launch
