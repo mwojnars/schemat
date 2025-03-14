c65e74ac62d508729bf58535c052e1122186c8c1
Marcin Wojnarski <mwojnars@ns.onet.pl>
2025-03-14 22:46:11 +0100

    DataBlock._encode_id()

--- schemat/db/block.js
+++ schemat/db/block.js
@@ -184,10 +184,12 @@ export class DataBlock extends Block {
     }
 
     async assert_unique(id, msg) {
-        let key = this.sequence.encode_id(id)
+        let key = this._encode_id(id)
         if (await this._storage.get(key))
             throw new DataConsistencyError(msg || "record with this ID already exists", {id})
     }
+
+    _encode_id(id) { return this.sequence.encode_id(id) }
 
     _annotate(json) {
         /* Append metadata (__meta) with ring & block ID to the JSON content of an object retrieved during select/update. */
@@ -219,7 +221,7 @@ export class DataBlock extends Block {
     async 'remote.select'(_, id, req) { return this._select(id, req) }
 
     async _select(id, req) {
-        let key = this.sequence.encode_id(id)
+        let key = this._encode_id(id)
         let data = await this._storage.get(key)         // JSON string
         if (data) return this._annotate(data)
         return this._move_down(req).select(id, req)
@@ -343,7 +345,7 @@ export class DataBlock extends Block {
            Otherwise, load the data associated with `id`, apply `edits` to it, and save a modified item
            in this block (if the ring permits), or forward the write request back to a higher ring. Return {id, data}.
          */
-        let key = this.sequence.encode_id(id)
+        let key = this._encode_id(id)
         let data = await this._storage.get(key)
         if (data === undefined) return this._move_down(req).update(id, edits, req)
 
@@ -372,7 +374,7 @@ export class DataBlock extends Block {
 
     async cmd_upsave(id, data, req) {
         /* Update, or insert an updated object, after the request `req` has been forwarded to a higher ring. */
-        let key = this.sequence.encode_id(id)
+        let key = this._encode_id(id)
         if (await this._storage.get(key))
             throw new DataConsistencyError('newly-inserted object with same ID discovered in a higher ring during upward pass of update', {id})
 
@@ -387,7 +389,7 @@ export class DataBlock extends Block {
     async _save(obj, prev = null) {
         let id = obj.id
         let data = obj.__json
-        let key = this.sequence.encode_id(id)
+        let key = this._encode_id(id)
 
         await this.put(key, data)
         await this.propagate_change(key, prev, obj)
@@ -400,7 +402,7 @@ export class DataBlock extends Block {
         /* Try deleting the `id`, forward to a lower ring if the id is not present here in this block.
            Log an error if the ring is read-only and the `id` is present here.
          */
-        let key = this.sequence.encode_id(id)
+        let key = this._encode_id(id)
         let data = await this._storage.get(key)
         if (data === undefined) return this._move_down(req).delete(id, req)
 
