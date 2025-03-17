859a599067e8d7cc95f1079ca07a5d7fe75ba6f0
Marcin Wojnarski <mwojnars@ns.onet.pl>
2025-03-17 23:07:47 +0100

    Schemat.object = alias for .target

--- schemat/web/client.js
+++ schemat/web/client.js
@@ -11,6 +11,7 @@ export class Client extends Schemat {
     /* Client-side global Schemat object. Used in .init_client() and .client_block() of the server-side Schemat. */
 
     target          // target web object that was addressed by the request, already loaded
+    object          // ... alias
 
     constructor(context_path) {
         let ctx = RequestContext.from_element(context_path)
@@ -29,8 +30,8 @@ export class Client extends Schemat {
             await this.get_loaded(rec.id)
 
         delete ctx.objects                                      // save memory (`ctx` is remembered in `schemat` as a global)
-        this.target = this.get_object(ctx.target)
-        this.target.assert_loaded()
+        this.object = this.target = this.get_object(ctx.target)
+        this.object.assert_loaded()
 
         // check()
     }
