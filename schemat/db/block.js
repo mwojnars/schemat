4e3961c661b4ccbf5d77bdac35bc7c2aea9c5466
Marcin Wojnarski <mwojnars@ns.onet.pl>
2025-03-20 01:11:26 +0100

    DataBlock.shard

--- schemat/db/block.js
+++ schemat/db/block.js
@@ -178,12 +178,12 @@ export class Block extends Agent {
 export class DataBlock extends Block {
     /* A Block that stores objects and provides the "insert" operation. */
 
+    // properties
+    shard
+    insert_mode             // if `compact`, new objects are inserted at the lowest possible ID in the block, possibly below _autoincrement; requires MemoryStorage
 
     _autoincrement = 1      // current maximum ID of records in this block; a new record is assigned id=_autoincrement+1 unless insert_mode='compact';
                             // transient field: NOT saved in the block's configuration in DB but re-initialized during block instantiation
-
-    // persistent properties
-    insert_mode             // if `compact`, new objects are inserted at the lowest possible ID in the block, possibly below _autoincrement; requires MemoryStorage
 
 
     async __init__() {
