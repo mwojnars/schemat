11fdd2ecc0e2230d832d8034ea5671b7c56cbcb0
Marcin Wojnarski <mwojnars@ns.onet.pl>
2025-06-12 22:31:58 +0200

    Application.action.insert_objects() relies on implicit tx.save() instead of direct db.insert() call

--- schemat/core/app.js
+++ schemat/core/app.js
@@ -231,7 +231,8 @@ export class Application extends WebObject {
            The respond is an array of IDs, one for each object created, in the same order as in the request.
            Additionally, DB records are passed implicitly through a Transaction context.
          */
-        return schemat.db.insert(data, opts)
+        return Array.isArray(data) ? data.map(d => WebObject.newborn(d)) : WebObject.newborn(data)
+        // return schemat.db.insert(data, opts)
     }
 
     'action.apply_edits'(id, ...edits) {
