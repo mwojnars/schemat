46b561ba4692bb067a4a1b8c3a2b8f2b9eaa6e71
Marcin Wojnarski <mwojnars@ns.onet.pl>
2025-06-14 22:22:06 +0200

    tests: draft

--- schemat/test/test.cjs
+++ schemat/test/test.cjs
@@ -250,6 +250,25 @@ describe('Schemat Tests', function () {
                 ['Category of objects', 'name', '__ttl', 'defaults', 'schema', 'Ring', 'Varia', 'schemat:Category'])
         })
 
+        // it('Varia: double insert', async function () {
+        //     await delay(200)
+        //     await test_page(page, `${DOMAIN}/$/id/2101`, '#page-main')
+        //     await delay(200)
+        //     let done = await page.evaluate(async () => {
+        //         let Varia = schemat.object
+        //         let a = Varia.new()
+        //         let b = a.ref = Varia.new({ref: a})
+        //         await schemat.save()
+        //         a = await a.reload()
+        //         b = await b.reload()
+        //         // a.delete_self()
+        //         // b.delete_self()
+        //         // await schemat.save()
+        //         return a.ref.is(b) && b.ref.is(a)
+        //     })
+        //     expect(done).to.be.true
+        // })
+
         it('Varia: load/insert/delete', async function () {
             // navigate to the Varia category page
             await test_page(page, `${DOMAIN}/$/id/2101`, '#page-main',
@@ -292,22 +311,6 @@ describe('Schemat Tests', function () {
             let updated_content = await extract_content(page)
             expect(updated_content).to.not.include(name)
         })
-
-        // it('Varia: double insert', async function () {
-        //     await delay(200)
-        //     await test_page(page, `${DOMAIN}/$/id/2101`, '#page-main')
-        //     await delay(200)
-        //     let done = await page.evaluate(async () => {
-        //         let Varia = schemat.object
-        //         let a = Varia.new()
-        //         let b = a.ref = Varia.new({ref: a})
-        //         await schemat.save()
-        //         // a.delete_self()
-        //         // b.delete_self()
-        //         // await schemat.save()
-        //     })
-        //     expect(done).to.be.true
-        // })
 
         it('rebuild_indexes', async function () {
             await delay(200)
