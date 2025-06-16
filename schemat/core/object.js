0faa7c8cd843b126530f2330e035b3c26ac1ce52
Marcin Wojnarski <mwojnars@ns.onet.pl>
2025-06-16 15:06:39 +0200

    minor

--- schemat/core/object.js
+++ schemat/core/object.js
@@ -565,7 +565,7 @@ export class WebObject {
         let fun    = caller?.match(/at (\S+)/)?.[1]     // function name of the caller
         let label  = `${this.__label}${fun ? '.'+fun+'()' : ''}`
         let title  = SERVER ? `${schemat.node?.id}/#${schemat.kernel?.worker_id} ${label} context ${schemat.db}` : label
-        console.log(title, '- debug stack trace:', ...args)
+        console.log(title, '-- debug stack trace:', ...args)
         console.log(lines.join('\n'))
     }
 
