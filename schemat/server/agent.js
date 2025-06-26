b2af5e75ab755a1f52b0ac4ce7361424b6a12d7e
Marcin Wojnarski <mwojnars@ns.onet.pl>
2025-06-26 23:59:12 +0200

    [Agent].schema.__concurrent

--- schemat/server/agent.js
+++ schemat/server/agent.js
@@ -37,6 +37,7 @@ export class Agent extends WebObject {
     get $frame()    { return this.__frame?.getStore() }
     get $state()    { return this.$frame?.state }
 
+    __concurrent    // if true, agent methods of this agent may execute concurrently
     num_workers     // number of concurrent workers per node that should execute this agent's microservice at the same time; -1 = "all available"
     hard_restart
     file_tag        // string to be included in names of files and directories
