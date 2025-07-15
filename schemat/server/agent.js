dc6dd700b1755bd17432857e80b1d36716964bd3
Marcin Wojnarski <mwojnars@ns.onet.pl>
2025-07-15 13:05:27 +0200

    minor

--- schemat/server/agent.js
+++ schemat/server/agent.js
@@ -19,13 +19,13 @@ export class Agent extends WebObject {
     get $state()    { return this.$frame?.state }
     get $role()     { return this.$frame?.role }
 
-    __ctx           // Database object that provides context of execution for __start__/__stop__/$agent.*()
-                    // methods ("user mode"); if missing, kernel's context (cluster-level DB) is used ("kernel mode");
+    __ctx           // Database object that provides context of execution for __start__/__stop__/$agent.*() methods
+                    // of this agent ("user mode"); if missing, kernel's context (cluster-level DB) is used ("kernel mode");
                     // behavior for $agent.*() calls can be modified with switch_context=true
 
+    switch_context      // if true, commands are executed in the caller's context not the agent's own context (__ctx)
     num_copies          // no. of concurrent copies of this agent to be deployed at a single node; -1 = "one per worker process"
     concurrent_calls    // if true, multiple calls to this agent may execute concurrently
-    switch_context      // if true, commands are executed in the caller's context not the agent's own context
     file_tag            // string to be included in names of local files and directories created by this agent
 
     get file_path() { throw new Error(`file_path not implemented for agent ${this}`) }
