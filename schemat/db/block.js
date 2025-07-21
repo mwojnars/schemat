fe0a4595ef584702f51182bf1ea54151b81bf0d0
Marcin Wojnarski <mwojnars@ns.onet.pl>
2025-07-21 22:10:16 +0200

    Monitor._commit_backfill() performs edit directly (no action)

--- schemat/db/block.js
+++ schemat/db/block.js
@@ -129,9 +129,9 @@ export class Monitor {
         fs.writeFileSync(this._backfill_path, report, {flush: true})
 
         // inform the destination sequence about new offset
-        this.dst.action.commit_backfill(this.src.keys_start, offset || this.src.keys_stop)   //prev
+        // this.dst.action.commit_backfill(this.src.keys_start, offset || this.src.keys_stop)   //prev
+        WebObject.remote(this.dst.id).edit.commit_backfill(this.src.keys_start, offset || this.src.keys_stop).save()
         // this.dst.as_remote().edit.*
-        // WebObject.remote(this.dst.id).edit.commit_backfill(this.src.keys_start, offset || this.src.keys_stop).save()
     }
 
     // _finalize_backfill() {
