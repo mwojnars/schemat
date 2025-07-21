3a7efaf0000b41e63d5893a9243ac8f7b4f271ce
Marcin Wojnarski <mwojnars@ns.onet.pl>
2025-07-21 22:47:05 +0200

    minor

--- schemat/db/block.js
+++ schemat/db/block.js
@@ -129,7 +129,9 @@ export class Monitor {
         fs.writeFileSync(this._backfill_path, report, {flush: true})
 
         // inform the destination sequence about new offset
-        this.dst.get_remote().edit.commit_backfill(this.src.keys_start, offset || this.src.keys_stop).save()   //prev
+        let start = this.src.keys_start     //prev
+        let stop  = offset || this.src.keys_stop
+        this.dst.get_remote().edit.commit_backfill(start, stop).save()
     }
 
     // _finalize_backfill() {
