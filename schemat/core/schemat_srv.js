a0f027ad4fedbe1d7baf66b1beabbd5b5eb03b3c
Marcin Wojnarski <mwojnars@ns.onet.pl>
2025-08-26 13:14:37 +0200

    ServerSchemat.in_transaction() does not tx.flush() automatically

--- schemat/core/schemat_srv.js
+++ schemat/core/schemat_srv.js
@@ -394,16 +394,16 @@ export class ServerSchemat extends Schemat {
 
     async in_transaction(callback, tx = this.tx, _return_tx = true) {
         /* Run callback() inside a new Transaction object, with TID inherited from `tx` or this.tx, or created anew.
-           If a new TID was assigned, the transaction is committed at the end. In either case, any local modifications
-           are saved (flushed) at the end. Returns: [result-of-callback(), transaction-object].
-           After the call, the transaction object contains info about the execution, esp. a list of records updated.
+           If a new TID was assigned, the transaction is committed at the end. After the call, the transaction object
+           contains info about the execution, esp. a list of records updated.
          */
         assert(this === schemat)
         let tid = tx?.tid
         tx = new ServerTransaction({tid})
         let result = await this._transaction.run(tx, async () => {
             let res = await callback()
-            if (tid) await tx.flush(); else await tx.commit()
+            if (!tid) await tx.commit()
+            // if (tid) await tx.flush(); else await tx.commit()
             return res
         })
         return _return_tx ? [result, tx] : result
