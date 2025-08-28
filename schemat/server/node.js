3f70f36d687f7e7d16d57dd7cb2cb76a1152ddd9
Marcin Wojnarski <mwojnars@ns.onet.pl>
2025-08-28 22:14:37 +0200

    minor

--- schemat/server/node.js
+++ schemat/server/node.js
@@ -287,7 +287,8 @@ export class Node extends Agent {
         await tcp_sender.start(this.tcp_retry_interval * 1000)
         await tcp_receiver.start(this.tcp_port)
 
-        // TODO: retrieve global_placements from cluster.$leader instead of relying on information stored in DB (can be outdated? or not?)
+        // TODO: retrieve global_placements from cluster.$leader instead of relying on information stored in DB (can be outdated?)
+        //       ... or, update global_placements from cluster.$leader right after initializing the node
         let global_placements = schemat.cluster.global_placements()
 
         return {tcp_sender, tcp_receiver, agents: this.agents, global_placements}
