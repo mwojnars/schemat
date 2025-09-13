47cae20125b81c69204dd6ad91131e7512a215c2
Marcin Wojnarski <mwojnars@ns.onet.pl>
2025-09-13 12:50:37 +0200

    comment

--- schemat/types/type.js
+++ schemat/types/type.js
@@ -526,8 +526,8 @@ export class REF extends Type {
     static options = {
         // category:  undefined,       // if present, the referenced object must be loaded and belong to `category`
         // exact:     false,           // if true, the objects must belong to this exact `category`, not any of its subcategories
-        strong:    false,           // if true, the referenced object is considered an essential part of the current one ("strong ownership")
-                                    // that should be removed automatically when the parent (or the link itself) is removed
+        strong:    false,       // if true, the referenced object is considered an essential part of the current one ("strong ownership")
+                                // and is removed automatically when the parent is deleted (but NOT when the parent is updated and the link alone is removed!)
     }
     static Widget = widgets.REF_Widget
 
