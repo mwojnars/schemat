0ddd1b4d826e10afa1d88567f741657e70da4c19
Marcin Wojnarski <mwojnars@ns.onet.pl>
2025-10-21 12:29:03 +0200

    svelte: fix

--- schemat/server/svelte_loader.js
+++ schemat/server/svelte_loader.js
@@ -14,7 +14,6 @@ export async function load(url, context, defaultLoad) {
     // use Svelte compiler
     const {js} = compile(source, {
         filename,
-        format: 'esm',
         css: 'injected',
         generate: 'dom'
     })
