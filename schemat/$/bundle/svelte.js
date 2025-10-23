d53c2ee7167a5f759be6313d2afcd780449e9c09
Marcin Wojnarski <mwojnars@ns.onet.pl>
2025-10-23 12:05:31 +0200

    /$/bundle/svelte includes many entry points

--- schemat/$/bundle/svelte.js
+++ schemat/$/bundle/svelte.js
@@ -2,9 +2,12 @@ import {bundle_dependencies} from "../../web/build.js"
 
 export async function GET({res}) {
     let deps = await bundle_dependencies([
-        `${schemat.PATH_PROJECT}/node_modules/svelte/src/index-client.js`
+        `${schemat.PATH_PROJECT}/node_modules/svelte/src/index-client.js`,
+        `${schemat.PATH_PROJECT}/node_modules/svelte/src/internal/client/index.js`,
+        `${schemat.PATH_PROJECT}/node_modules/svelte/src/internal/disclose-version.js`,
+        `${schemat.PATH_PROJECT}/node_modules/svelte/src/internal/flags/legacy.js`,
     ])
-    schemat._print(`/$/bundle/svelte deps:\n`, deps.files)
+    schemat._print(`/$/bundle/svelte deps (${deps.files.length}):\n`, deps.files)
     schemat._print(`/$/bundle/svelte code: ${deps.bundle.length} bytes`)
     res.type('js')
     res.send(deps.bundle)
