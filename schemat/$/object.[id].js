bb5e626417735d6b76fe4f123afeba0cd844f890
Marcin Wojnarski <mwojnars@ns.onet.pl>
2025-10-24 19:37:11 +0200

    /$/object/[id] -- draft

--- schemat/$/object.[id].js
+++ schemat/$/object.[id].js
@@ -4,5 +4,7 @@ export async function GET(request, {id}) {
     let obj = await schemat.load(id)
     let View = obj.is_category() ? CategoryInspectView : InspectView
     let page = new ReactPage(View)
+    request.target = obj
+    request.endpoint = "GET.inspect"    // FIXME
     return page.handle(obj, request)
 }
