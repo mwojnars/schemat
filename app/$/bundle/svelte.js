b4a97982623b965fb5791a5382404622feb84760
Marcin Wojnarski <mwojnars@ns.onet.pl>
2025-10-27 19:33:06 +0100

    cleanup

--- app/$/bundle/svelte.js
+++ app/$/bundle/svelte.js
@@ -1,12 +1,6 @@
 import {bundle_dependencies} from "#schemat/web/build.js"
 
 export async function GET({res}) {
-    // let deps = await bundle_dependencies([
-    //     `${schemat.PATH_PROJECT}/node_modules/svelte/src/index-client.js`,
-    //     `${schemat.PATH_PROJECT}/node_modules/svelte/src/internal/client/index.js`,
-    //     `${schemat.PATH_PROJECT}/node_modules/svelte/src/internal/disclose-version.js`,
-    //     `${schemat.PATH_PROJECT}/node_modules/svelte/src/internal/flags/legacy.js`,
-    // ])
     let deps = await bundle_dependencies([
         `${schemat.PATH_PROJECT}/node_modules/svelte/src/index-client.js`,
         ...schemat.app._svelte_imports
